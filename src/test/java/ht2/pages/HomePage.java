434c5a873c1b09ae0182fd52f0e2172f48ee049a
New Sheriff in Town <dimashedin51@gmail.com>
2021-09-06 23:44:50 +0300

    add ht3

--- src/test/java/ht2/pages/HomePage.java
+++ src/test/java/ht2/pages/HomePage.java
@@ -3,13 +3,9 @@ package ht2.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.List;
-import java.util.function.Function;
 
 public final class HomePage extends AbstractPage {
 
