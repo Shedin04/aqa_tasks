c2442385832b6ca9e63754afabb0a4bb4bd64e6d
New Sheriff in Town <dimashedin51@gmail.com>
2021-09-08 11:05:29 +0300

    h3:update tests

--- src/test/java/ht3/pages/MainPage.java
+++ src/test/java/ht3/pages/MainPage.java
@@ -51,30 +51,42 @@ public final class MainPage extends BasePage {
         return this;
     }
 
-    public String selectDeparture(String request, int number){
+    public MainPage selectDeparture(String request, int number){
         waitFor(departureField);
         departureField.clear();
         departureField.click();
         departureField.sendKeys(request);
         waitAndClick(number, resultOfDrop);
+        return this;
+    }
+
+    public String getDeparture(){
         return departureField.getAttribute("value");
     }
 
-    public String selectArrival(String request,int number){
+    public MainPage selectArrival(String request,int number){
         waitFor(arrivalField);
         arrivalField.clear();
         arrivalField.click();
         arrivalField.sendKeys(request);
         waitAndClick(number, resultOfDrop);
+        return this;
+    }
+
+    public String getArrival(){
         return arrivalField.getAttribute("value");
     }
 
-    public String selectDates(int dayDeparture, int monthDeparture, int dayArrival, int monthArrival){
+    public MainPage selectDates(int dayDeparture, int monthDeparture, int dayArrival, int monthArrival){
         waitFor(tripStart);
         tripStart.click();
         waitFor(activeDays.get(0));
         activeDays.stream().filter(day -> day.getText().equals(String.valueOf(dayDeparture))).collect(Collectors.toList()).get(monthDeparture-1).click();
         activeDays.stream().filter(day -> day.getText().equals(String.valueOf(dayArrival))).collect(Collectors.toList()).get(monthArrival-1).click();
+        return this;
+    }
+
+    public String getDates(){
         return dates.get(0).getAttribute("value") + " | " + dates.get(1).getAttribute("value");
     }
 }
