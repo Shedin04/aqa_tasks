8b390045c4ecaba9a412e906023a595b0faf1ddc
New Sheriff in Town <dimashedin51@gmail.com>
2021-09-09 13:11:34 +0300

    update tests

--- src/test/java/ht3/tests/TicketsSearchTests.java
+++ src/test/java/ht3/tests/TicketsSearchTests.java
@@ -1,17 +1,24 @@
 package ht3.tests;
 
 import ht3.pages.MainPage;
+import ht3.pages.TicketsSearchPage;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TicketsSearchTests extends BaseTest{
 
+    @BeforeMethod(description = "Open search page")
+    private void openPage(){
+        new MainPage(driver).openPage()
+                .selectDeparture("Хар", 0).selectArrival("Ст",0)
+                .selectDates(26, 1,6,2).bookingComFlag()
+                .clickToFindTickets();
+    }
+
     @Test(description = "Check popular ticket filters")
     public void checkPopularFilters(){
         String buttonName = "без багажу";
-        Assert.assertEquals(new MainPage(driver).openPage()
-                .selectDeparture("Бор", 0).selectArrival("Ла",2)
-                .selectDates(23, 1,8,2).bookingComFlag()
-                .clickToFindTickets().clickPopularFiltersButton(buttonName).checkSuite(), 10);
+        Assert.assertEquals(new TicketsSearchPage(driver).clickPopularFiltersButton(buttonName).checkSuite(), 3);
     }
 }
